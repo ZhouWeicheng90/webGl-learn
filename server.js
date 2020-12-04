150e1d5213000001af4484dd72d40bf043635b96
Enne5w4 <enne5w4@126.com>
2020-12-04 18:43:20 +0800

    feat:

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app1.listen(1240, () => {
     console.log('eg:http://localhost:1240/webGL/gl5-texture2卷积.html')
     console.log('eg:http://localhost:1240/webGL/gl6-texture3效果叠加.html')   
     console.log('eg:http://localhost:1240/transform3D/2d.html') 
+    //  http://localhost:1240/transform3D/3d.html
 })
